9cbd32cb04bbe42daf18685951c16a048927519a
Rajeswari koncha <rajeswarikoncha06@gmail.com>
2026-07-02 18:01:31 +0530

    datatpes.py

---
+++ datatypes.py
@@ -0,0 +1,73 @@
+# Converting One Data Type to Another (24 Types)
+
+# 1. int -> float
+print(float(10))
+
+# 2. int -> str
+print(str(10))
+
+# 3. int -> bool
+print(bool(10))
+
+# 4. float -> int
+print(int(10.75))
+
+# 5. float -> str
+print(str(10.75))
+
+# 6. float -> bool
+print(bool(10.75))
+
+# 7. str -> int
+print(int("100"))
+
+# 8. str -> float
+print(float("100.25"))
+
+# 9. str -> bool
+print(bool("Python"))
+
+# 10. bool -> int
+print(int(True))
+
+# 11. bool -> float
+print(float(False))
+
+# 12. bool -> str
+print(str(True))
+
+# 13. list -> tuple
+print(tuple([1, 2, 3]))
+
+# 14. tuple -> list
+print(list((4, 5, 6)))
+
+# 15. list -> set
+print(set([1, 2, 2, 3]))
+
+# 16. set -> list
+print(list({1, 2, 3}))
+
+# 17. tuple -> set
+print(set((1, 2, 3)))
+
+# 18. set -> tuple
+print(tuple({1, 2, 3}))
+
+# 19. str -> list
+print(list("Python"))
+
+# 20. list -> str
+print("".join(['P', 'y', 't', 'h', 'o', 'n']))
+
+# 21. str -> tuple
+print(tuple("Python"))
+
+# 22. str -> set
+print(set("Python"))
+
+# 23. dict -> list (keys)
+print(list({'a': 1, 'b': 2, 'c': 3}))
+
+# 24. list of tuples -> dict
+print(dict([('a', 1), ('b', 2), ('c', 3)]))
